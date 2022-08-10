bc8f8cf2821caba6752dfe11c18624439faadbcd
sugyeong-lee <waterscape2021@naver.com>
2022-08-10 23:34:23 +0900

    22/08/10

---
+++ 유노코딩/미니 프로젝트/로또 번호 추첨/script.js
@@ -0,0 +1,12 @@
+const numbersDiv = document.querySelector(".numbers")
+const drawButton = document.querySelector("#draw")
+const resetButton = document.querySelector("#reset")
+
+const lottoNumbers = []
+const colors = ["tomato", "teal", "orange", "purple", "blue"]
+
+function paintNumber(number){
+    const eachNumDiv = document.createElement("div")
+    let colorIndex = Math.floor(number / 10)
+    each
+}
