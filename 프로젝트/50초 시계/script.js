4270d0f136143a096579a5cdfe6ec7633f80f16f
sugyeong-lee <waterscape2021@naver.com>
2022-08-18 18:53:36 +0900

    22/08/18

--- 프로젝트/50초 시계/script.js
+++ 프로젝트/50초 시계/script.js
@@ -1,27 +1,26 @@
 function printClock() {
     
-    var clock = document.getElementById("clock");            // 출력할 장소 선택
+    var clock = document.getElementById("clock");
     var currentDate = new Date();
-    var amPm = 'AM'; // 초기값 AM                                     // 현재시간
+    var amPm = 'AM';          
     var Hours = addZeros(currentDate.getHours(),2); 
     var Minute = addZeros(currentDate.getMinutes() ,2);
     var Seconds =  addZeros(currentDate.getSeconds(),2);
     
-    if(Hours >= 12){ // 시간이 12보다 클 때 PM으로 세팅, 12를 빼줌
+    if(Hours >= 12){
     	amPm = 'PM';
     	Hours = addZeros(Hours - 12,2);
     }
 
-    if(Seconds >= 50){// 50초 이상일 때 색을 변환해 준다.
-        Hours = '<span style="color:#de1951;">'+Hours+'</span>'
+    if(Seconds >= 50){
         Seconds = '<span style="color:#de1951;">'+Seconds+'</span>'
     }
-    clock.innerHTML = Hours+":"+Minute+":"+Seconds +" <span style='font-size:50px;'>"+ amPm+"</span>"; //날짜를 출력해 줌
+    clock.innerHTML = Hours+":"+Minute+":"+Seconds +" <span style='font-size:50px;'>"+ amPm+"</span>";
     
-    setTimeout("printClock()",1000);         // 1초마다 printClock() 함수 호출
+    setTimeout("printClock()",1000);
 }
 
-function addZeros(num, digit) { // 자릿수 맞춰주기
+function addZeros(num, digit) {
 	  var zero = '';
 	  num = num.toString();
 	  if (num.length < digit) {
@@ -30,4 +29,8 @@ function addZeros(num, digit) { // 자릿수 맞춰주기
 	    }
 	  }
 	  return zero + num;
+}
+
+function record(num, digit) {
+  clock.innerHTML = Hours+":"+Minute+":"+Seconds +" <span style='font-size:50px;'>"+ amPm+"</span>";
 }
